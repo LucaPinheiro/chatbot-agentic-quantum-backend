c3161a329a011dfbac33a275599108999765f8fd
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-04-20 19:20:52 -0300

    Adiciona arquivos iniciais para a estrutura do projeto, incluindo módulos principais, de domínio, infraestrutura e pipelines, com documentação básica e definições de versão.

---
+++ app/infra/external/__init__.py
@@ -0,0 +1,9 @@
+from .aws import DynamoConfig, DynamoDBResources, SQSResources, RekognitionResources, S3Resources
+
+__all__ = [
+    'DynamoConfig',
+    'DynamoDBResources',
+    'SQSResources',
+    'RekognitionResources',
+    'S3Resources'
+]
---
+++ app/pipelines/__init__.py
@@ -0,0 +1,4 @@
+"""
+Módulo de pipelines da aplicação.
+Contém as definições e implementações dos pipelines de processamento.
+""" 
