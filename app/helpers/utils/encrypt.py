4dda26f2d53ba443b4fa244e7a838655d9403895
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-04-21 01:01:04 -0300

    Atualiza a classe Encrypt para utilizar um salt gerado pelo bcrypt ao criar hashes de senhas, melhorando a segurança no armazenamento de senhas.

--- app/helpers/utils/encrypt.py
+++ app/helpers/utils/encrypt.py
@@ -1,17 +1,15 @@
 import bcrypt
-
 from app.core.settings import load_settings
 
 
 class Encrypt:
     settings = load_settings()
 
-    
     @staticmethod
     def hash_password(password: str) -> str:
-        encrypt_key = settings.ENCRYPT_KEY.encode()
-        return bcrypt.hashpw(password.encode(), encrypt_key).decode()
-    
+        salt = bcrypt.gensalt()
+        return bcrypt.hashpw(password.encode(), salt).decode()
+
     @staticmethod
     def verify_password(password: str, hashed_password: str) -> bool:
         return bcrypt.checkpw(password.encode(), hashed_password.encode())
