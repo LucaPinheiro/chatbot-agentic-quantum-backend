59b1cf4631f4bd0e19e171eb9826fa87fb0cdc47
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-04-21 01:01:54 -0300

    Adiciona o arquivo models.py com a implementação das classes Base e User, definindo a estrutura do modelo de dados para usuários, incluindo atributos como id, nome, email e senha, facilitando a persistência de dados no banco de dados.

---
+++ app/models/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, String
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    __abstract__ = True
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__} {self.__dict__}>"
+    
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(String, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
+
