e465dbbb171b7c4d90498365e5f806ec1006da74
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-04-22 00:43:40 -0300

    Adiciona campos de data de criação e nível de permissão à classe CreateUserRequest, aprimorando a estrutura de dados para a criação de usuários na aplicação.

--- app/schemas/create_user.py
+++ app/schemas/create_user.py
@@ -1,9 +1,12 @@
+import datetime
 from pydantic import BaseModel, EmailStr, Field
 
 class CreateUserRequest(BaseModel):
     name: str             = Field(..., min_length=2, max_length=80)
     email: EmailStr
     password: str         = Field(..., min_length=6, max_length=128)
+    created_at: datetime.datetime
+    permission: int
 
 class CreateUserResponse(BaseModel):
     id: str
