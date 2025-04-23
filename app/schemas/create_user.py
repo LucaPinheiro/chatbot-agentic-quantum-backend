2fb659f746ef0de26625093164bb1f6820ffd1ff
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-04-23 00:27:18 -0300

    Altera o campo id para user_id na resposta de criação de usuário, melhorando a consistência na nomenclatura dos atributos. Remove o campo created_at da classe CreateUserRequest, simplificando a estrutura do modelo.

--- app/schemas/create_user.py
+++ app/schemas/create_user.py
@@ -5,10 +5,9 @@ class CreateUserRequest(BaseModel):
     name: str             = Field(..., min_length=2, max_length=80)
     email: EmailStr
     password: str         = Field(..., min_length=6, max_length=128)
-    created_at: datetime.datetime
     permission: int
 
 class CreateUserResponse(BaseModel):
-    id: str
+    user_id: str
     name: str
     email: EmailStr
