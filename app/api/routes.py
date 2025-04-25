386ead30968bf537aaff83e44e1781f7a5a868df
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-04-25 00:30:27 -0300

    Adiciona o roteador para consulta de usuário por e-mail, integrando-o à lista de roteadores existentes. Melhora a estrutura da API ao expandir as funcionalidades disponíveis.

--- app/api/routes.py
+++ app/api/routes.py
@@ -3,10 +3,11 @@ from fastapi import APIRouter
 from app.api.endpoints.health.health_check import router as health_check_router
 from app.api.endpoints.user.create_user import router as create_user_router
 from app.api.endpoints.user.login import router as login_router
+from app.api.endpoints.user.get_user_by_email import router as get_user_by_email_router
 # from app.api.v1.endpoints.user import router as user_router
 
 routers = APIRouter()
-router_list = [health_check_router, create_user_router, login_router]
+router_list = [health_check_router, create_user_router, login_router, get_user_by_email_router]
 
 for router in router_list:
     router.tags = routers.tags.append("v1")
