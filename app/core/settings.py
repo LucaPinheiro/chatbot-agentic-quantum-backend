1e12413e9adbdafcb187a203ec380d773f95858f
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-05-03 19:46:56 -0300

    Refactor settings.py: add SQS queue URL configuration

--- app/core/settings.py
+++ app/core/settings.py
@@ -47,6 +47,9 @@ class Settings(BaseSettings):
 
     redis_host: str = "redis"
     redis_port: int = 6379
+    
+    # --------------- SQS QUEUES ---------------
+    sqs_queue_url: str = ""  
 
     # ─────────────── Overrides (opcional) ───────────────
     postgres_url_prefix: str | None = None
