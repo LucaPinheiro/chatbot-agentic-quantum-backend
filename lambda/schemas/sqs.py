bdd06649dcd15e7a02989b0e0edaea36ab8c879d
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-05-03 16:51:26 -0300

    Add SQSMessage and SQSEvent classes for handling SQS messages

---
+++ lambda/schemas/sqs.py
@@ -0,0 +1,21 @@
+import json
+from pydantic import BaseModel
+from typing import Literal, Optional
+from datetime import datetime
+
+
+class SQSMessage(BaseModel):
+    session_id: str
+    summary_cutoff: Optional[str]
+    message_group_id: Literal["summarization"]
+
+    def to_json(self) -> str:
+        return self.model_dump_json()
+
+
+class SQSEvent:
+    def __init__(self, raw_event: dict):
+        self.records = [json.loads(r["body"]) for r in raw_event["Records"]]
+
+    def __iter__(self):
+        return iter(self.records)
