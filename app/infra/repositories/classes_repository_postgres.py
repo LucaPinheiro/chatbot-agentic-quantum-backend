8272d1daefc8655282575d9a353a63acddf4cc24
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-05-05 00:39:48 -0300

    Refactor ClassesRepositoryPostgres to use ClassModel instead of Classes in type hints and query methods

--- app/infra/repositories/classes_repository_postgres.py
+++ app/infra/repositories/classes_repository_postgres.py
@@ -1,15 +1,15 @@
 from typing import List
 from app.domain.interfaces.classes_repository import IClassesRepository
-from app.models.models import Classes
+from app.models.models import ClassModel
 from sqlalchemy.orm import Session
 
 
 class ClassesRepositoryPostgres(IClassesRepository):
     def __init__(self, db: Session):
         self.db = db
-    def get_classes_by_group(self, group_id: str) -> List[Classes]:
-        classes = self.db.query(Classes).filter(Classes.group_id == group_id).all()
+    def get_classes_by_group(self, group_id: str) -> List[ClassModel]:
+        classes = self.db.query(ClassModel).filter(ClassModel.group_id == group_id).all()
         if not classes:
             return []
         print(f"Consultando banco para group_id={classes}")
-        return [Classes.from_orm(classes) for classes in classes]
+        return [ClassModel.from_orm(classes) for classes in classes]
