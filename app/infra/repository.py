d184c504a3d2a571c536ce731ef32fc03bdb7c12
Breno1101 <brenocoutinhorodrigues@gmail.com>
2025-05-05 00:27:00 -0300

    Adiciona suporte para repositório de classes na inicialização do repositório

--- app/infra/repository.py
+++ app/infra/repository.py
@@ -2,12 +2,14 @@ from sqlalchemy import NullPool, create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.exc import SQLAlchemyError
 
+from app.domain.interfaces.classes_repository import IClassesRepository
 from app.domain.interfaces.file_repository import IFileRepository
 from app.domain.interfaces.user_repository import IUserRepository
 
 from app.core.settings import load_settings, StageEnum
 from app.helpers.exceptions.exceptions import DatabaseException
 from app.infra.mocks.user_repository_mock import UserRepoMock
+from app.infra.repositories.classes_repository_postgres import ClassesRepositoryPostgres
 from app.infra.repositories.file_repository_s3 import FileRepositoryS3
 from app.infra.repositories.user_repository_postgres import UserRepositoryPostgres
 
@@ -17,21 +19,24 @@ settings = load_settings()
 class Repository:
     user_repo: IUserRepository
     file_repo: IFileRepository
+    classes_repo:IClassesRepository
 
-    def __init__(self, user_repo: bool = False, file_repo: bool = False):
+    def __init__(self, user_repo: bool = False, file_repo: bool = False, classes_repo: bool = False):
         self.session = None
 
         if settings.stage == StageEnum.test:
             self._initialize_mock_repositories(user_repo)
         else:
-            self._initialize_real_repositories(user_repo, file_repo)
+            self._initialize_real_repositories(user_repo, file_repo, classes_repo)
 
     def _initialize_mock_repositories(self, user_repo):
         if user_repo:
             self.user_repo = UserRepoMock()
 
-    def _initialize_real_repositories(self, user_repo, file_repo):
+    def _initialize_real_repositories(self, user_repo, file_repo, classes_repo):
         self.session = self.__connect_db()
+        if classes_repo:
+            self.classes_repo = ClassesRepositoryPostgres(self.session)
 
         if user_repo:
             self.user_repo = UserRepositoryPostgres(self.session)
