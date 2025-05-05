1b3f22c8fe21d7048311a4aa67330624c0e84fa8
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-05-05 00:39:45 -0300

    Update type hint in IClassesRepository to use ClassModel instead of Classes

--- app/domain/interfaces/classes_repository.py
+++ app/domain/interfaces/classes_repository.py
@@ -1,10 +1,10 @@
 from abc import abstractmethod
 from typing import List, Optional
 
-from app.domain.entities.classes import Classes
+from app.domain.entities.classes import ClassModel
 
 
 class IClassesRepository:
     @abstractmethod
-    def get_classes_by_group(self, group_id: str) -> Optional[List[Classes]]:
+    def get_classes_by_group(self, group_id: str) -> Optional[List[ClassModel]]:
         pass
