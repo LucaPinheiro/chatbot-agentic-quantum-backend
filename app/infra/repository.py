4b48a54bac4ac4b4f9106974e2fd42cd260e1f84
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-05-13 23:19:44 -0300

    Adicionar suporte ao repositório de inscrição em grupo na classe Repository

--- app/infra/repository.py
+++ app/infra/repository.py
@@ -6,6 +6,7 @@ from app.domain.interfaces.chat_repository import IChatRepository
 from app.domain.interfaces.class_topics_repository import IClassTopicsRepository
 from app.domain.interfaces.classes_repository import IClassesRepository
 from app.domain.interfaces.file_repository import IFileRepository
+from app.domain.interfaces.group_enrollment_repository import IGroupEnrollmentRepository
 from app.domain.interfaces.group_repository import IGroupRepository
 from app.domain.interfaces.session_repository import ISessionRepository
 from app.domain.interfaces.user_repository import IUserRepository
@@ -18,6 +19,7 @@ from app.infra.repositories.chat_repository_dynamo import ChatRepositoryDynamo
 from app.infra.repositories.class_topics_repository_postgres import ClassTopicsRepositoryPostgres
 from app.infra.repositories.classes_repository_postgres import ClassesRepositoryPostgres
 from app.infra.repositories.file_repository_s3 import FileRepositoryS3
+from app.infra.repositories.group_enrollment_postgres import GroupEnrollmentPostgres
 from app.infra.repositories.group_repository_postgres import GroupRepositoryPostgres
 from app.infra.repositories.session_repository_postgres import SessionRepositoryPostgres
 from app.infra.repositories.user_repository_postgres import UserRepositoryPostgres
@@ -33,20 +35,25 @@ class Repository:
     classes_repo: IClassesRepository
     class_topics_repo: IClassTopicsRepository
     group_repo: IGroupRepository
+    group_enrollment_repo: IGroupEnrollmentRepository
 
-    def __init__(self, user_repo: bool = False, file_repo: bool = False, session_repo: bool = False, chat_repo: bool = False, classes_repo: bool = False, class_topics_repo: bool = False, group_repo: bool = False):
+    def __init__(self, user_repo: bool = False, file_repo: bool = False, session_repo: bool = False,
+                 chat_repo: bool = False, classes_repo: bool = False, class_topics_repo: bool = False,
+                 group_repo: bool = False, group_enrollment_repo: bool = False):
         self.session = None
 
         if settings.stage == StageEnum.test:
             self._initialize_mock_repositories(user_repo)
         else:
-            self._initialize_real_repositories(user_repo, file_repo, session_repo, chat_repo, classes_repo, class_topics_repo, group_repo)
+            self._initialize_real_repositories(user_repo, file_repo, session_repo, chat_repo, classes_repo,
+                                               class_topics_repo, group_repo, group_enrollment_repo)
 
     def _initialize_mock_repositories(self, user_repo):
         if user_repo:
             self.user_repo = UserRepoMock()
 
-    def _initialize_real_repositories(self, user_repo, file_repo, session_repo, chat_repo, classes_repo, class_topics_repo, group_repo):
+    def _initialize_real_repositories(self, user_repo, file_repo, session_repo, chat_repo, classes_repo,
+                                      class_topics_repo, group_repo, group_enrollment_repo):
         self.session = self.__connect_db()
         
         if classes_repo:
@@ -66,6 +73,9 @@ class Repository:
         
         if group_repo:
             self.group_repo = GroupRepositoryPostgres(self.session)
+            
+        if group_enrollment_repo:
+            self.group_enrollment_repo = GroupEnrollmentPostgres(self.session)
             
         if chat_repo:
             dynamo_config = DynamoConfig(table_name=settings.dynamodb_table_messages)
