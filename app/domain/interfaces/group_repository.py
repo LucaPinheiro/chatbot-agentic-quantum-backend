c52d3ba2318665b827e3d61416d95ae5c18d65fd
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-05-13 23:19:41 -0300

    Adicionar nova linha ao final do arquivo group_repository.py

--- app/domain/interfaces/group_repository.py
+++ app/domain/interfaces/group_repository.py
@@ -6,6 +6,14 @@ class IGroupRepository:
     @abstractmethod
     def delete_group(self, name: str) -> object:
         pass
+    
     @abstractmethod
     def create_group(self, group: Group) -> Group:
+        pass
+    
+    @abstractmethod
+    def is_manager_group(self, group_id: str, user_id: str) -> bool:
+        """
+        Verifica se o usuário (professor ou admin) é o gerente do grupo
+        """
         pass
