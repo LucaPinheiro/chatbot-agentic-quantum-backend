680b15078147d1eec3ef4468dc868c58347d5cfc
Breno1101 <brenocoutinhorodrigues@gmail.com>
2025-05-13 21:53:51 -0300

    Adicionar métodos abstratos para exclusão e recuperação de todas as aulas na interface IClassesRepository

--- app/domain/interfaces/classes_repository.py
+++ app/domain/interfaces/classes_repository.py
@@ -1,10 +1,19 @@
 from abc import abstractmethod
-from typing import List, Optional
+from typing import List, Optional, Union
 
 from app.domain.entities.classes import ClassModel
+from app.schemas.update_class import UpdateClassResponse
 
 
 class IClassesRepository:
     @abstractmethod
     def get_classes_by_group(self, group_id: str) -> Optional[List[ClassModel]]:
+        pass
+    @abstractmethod
+    def delete_class(self, group_id: str, title: str) -> object:
+        pass
+    @abstractmethod
+    def get_all_classes(self) -> List[ClassModel]:
+        pass
+    def update_class(self, class_id: str) -> Union[UpdateClassResponse, dict]:
         pass
