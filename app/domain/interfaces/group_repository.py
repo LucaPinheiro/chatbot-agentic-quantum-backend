aae2d425ff4e03bc1780c5dce6ae8d9430fadb7d
Breno1101 <brenocoutinhorodrigues@gmail.com>
2025-05-13 21:52:59 -0300

    Adicionar método de exclusão de grupo à interface IGroupRepository

--- app/domain/interfaces/group_repository.py
+++ app/domain/interfaces/group_repository.py
@@ -6,3 +6,6 @@ class IGroupRepository:
     @abstractmethod
     def delete_group(self, name: str) -> object:
         pass
+    @abstractmethod
+    def create_group(self, group: Group) -> Group:
+        pass
