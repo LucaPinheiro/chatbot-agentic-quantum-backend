2e9ffc921b7c74a6cbeed88a7eb095b5096228c6
Breno1101 <brenocoutinhorodrigues@gmail.com>
2025-05-14 23:26:10 -0300

    Adicionar método abstrato para atualização de grupos e obtenção de grupo por ID

--- app/domain/interfaces/group_repository.py
+++ app/domain/interfaces/group_repository.py
@@ -1,5 +1,7 @@
 from abc import abstractmethod
+from typing import Optional, Union
 from app.models.models import Group
+from app.schemas.update_group import UpdateGroupResponse
 
 
 class IGroupRepository:
@@ -8,4 +10,17 @@ class IGroupRepository:
         pass
     @abstractmethod
     def create_group(self, group: Group) -> Group:
+        pass
+    @abstractmethod
+    def update_group(
+        self,
+        group_id: str,
+        name: Optional[str],
+        year_semester: Optional[int],
+        status: Optional[bool],
+        user_id: Optional[str]
+    ) -> Union[UpdateGroupResponse, dict]:
+        pass
+    @abstractmethod
+    def get_group_by_id(self, group_id: str) -> Optional[Group]:
         pass
