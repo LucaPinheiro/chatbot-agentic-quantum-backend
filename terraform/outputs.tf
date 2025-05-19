d647475a14cfb10ff399045d12dc178a01db4fb6
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-05-19 00:23:07 -0300

    Comment out unused output variables for lambda and SQS in outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,18 @@
-output "lambda_arn" {
-  value = aws_lambda_function.summarize_lambda.arn
+# output "lambda_arn" {
+#   value = aws_lambda_function.summarize_lambda.arn
+# }
+
+# output "sqs_url" {
+#   value = aws_sqs_queue.chatbot_summary_queue.url
+# }
+
+output "app_runner_arn" {
+  value       = aws_apprunner_service.fastapi_service.arn
+  description = "ARN do serviço App Runner"
 }
 
-output "sqs_url" {
-  value = aws_sqs_queue.chatbot_summary_queue.url
+
+output "app_runner_url" {
+  value       = aws_apprunner_service.fastapi_service.service_url
+  description = "URL pública HTTPS do serviço App Runner"
 }
