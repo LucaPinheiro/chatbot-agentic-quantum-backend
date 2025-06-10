93812f5733415af7d9eb4c1228c75c446c346b04
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-06-10 00:30:06 -0300

    Remove commented-out update_class method from IClassesRepository interface for clarity

--- app/domain/interfaces/classes_repository.py
+++ app/domain/interfaces/classes_repository.py
@@ -16,22 +16,15 @@ class IClassesRepository:
     @abstractmethod
     def get_all_classes(self) -> List[ClassModel]:
         pass
+    # @abstractmethod
+    # def update_class(self,
+    #                  class_id: str,
+    #                  group_id: Optional[str],
+    #                  title: Optional[str],
+    #                  pdf_url: Optional[str],
+    #                  status: Optional[bool],
+    #                  order: Optional[int]) -> Union[UpdateClassResponse, dict]:
+    #     pass
     @abstractmethod
-    def update_class(self,
-                     class_id: str,
-                     group_id: Optional[str],
-                     title: Optional[str],
-                     pdf_url: Optional[str],
-                     status: Optional[bool],
-                     order: Optional[int]) -> Union[UpdateClassResponse, dict]:
-        pass
-    @abstractmethod
-    def create_class(
-        self,
-        group_id: str,
-        title: str,
-        pdf_url: str,
-        class_topics: List[ClassTopicInput],
-        user_id: str,
-    ) -> CreateClassResponse:
+    def create_class(_class: ClassModel) -> None:
         pass
