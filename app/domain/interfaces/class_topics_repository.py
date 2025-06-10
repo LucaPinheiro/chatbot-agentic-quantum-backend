3c46b04bb841fc0fdc2805f84515721e171b5df6
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-06-10 00:30:03 -0300

    Add create_class_topic method to IClassTopicsRepository interface

--- app/domain/interfaces/class_topics_repository.py
+++ app/domain/interfaces/class_topics_repository.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 from typing import Dict, List, Union
 
+from app.domain.entities.class_topics import ClassTopics
+
 class IClassTopicsRepository(ABC):
     @abstractmethod
     def get_class_progress_percentual_by_class(self, class_id: str, user_id: str, class_topics_id: str) -> Dict:
@@ -9,5 +11,11 @@ class IClassTopicsRepository(ABC):
     def delete_topics_by_id(self, class_topics_id: Union[str, List[str]], class_id: Union[str, List[str]], topic: Union[str, List[str]]) -> Dict:
         """
         Deleta os tópicos de uma aula
+        """
+        pass
+    @abstractmethod
+    def create_class_topic(self, class_topic: ClassTopics) -> ClassTopics:
+        """
+        Cria um tópico de aula
         """
         pass
