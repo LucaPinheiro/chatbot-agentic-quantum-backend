078116eed8dd94074aff31d7757105ddb1343319
Breno1101 <brenocoutinhorodrigues@gmail.com>
2025-06-09 22:40:33 -0300

    adiciona endpoint para obter todas as classes de um usuário

---
+++ app/api/endpoints/classes/get_all_classes_by_user.py
@@ -0,0 +1,61 @@
+from typing import List
+from fastapi import APIRouter, HTTPException, Security
+from app.core.permissions import RequirePermission
+from app.domain.interfaces.classes_repository import IClassesRepository
+from app.domain.interfaces.user_repository import IUserRepository
+from app.helpers.enums.enums import PermissionLevelEnum
+from app.helpers.exceptions.exceptions import NotFoundException
+from app.infra.repository import Repository
+from app.schemas.get_all_classes import GetAllClassesRequest, GetAllClassesResponse
+from app.schemas.get_all_classes_by_user import GetAllClassesByUserRequest, GetAllClassesByUserResponse
+from app.schemas.get_all_users import GetAllUsersRequest, GetAllUsersResponse
+from app.schemas.token import TokenUser
+
+
+router = APIRouter()
+
+
+class UseCase:
+    repository: Repository
+    classes_repo: IClassesRepository
+
+    def __init__(self):
+        self.repository = Repository(classes_repo=True)
+        self.classes_repo = self.repository.classes_repo
+
+    def execute(self, schema: GetAllClassesByUserRequest) -> List[GetAllClassesByUserResponse]:
+        classes = self.classes_repo.get_all_classes_by_user(schema.user_id)
+        return [
+            GetAllClassesByUserResponse(
+                class_id=cls.class_id,
+                group_id=cls.group_id,
+                title=cls.title,
+                pdf_url=cls.pdf_url,
+                status=cls.status,
+                last_access_class=cls.last_access_class,
+                created_at=cls.created_at,
+                order=cls.order
+            )
+            for cls in classes
+        ]
+    
+class Controller:
+    def __init__(self, use_case: UseCase):
+        self.use_case = use_case
+
+    def handle(self, request: GetAllClassesByUserRequest) -> List[GetAllClassesByUserResponse]:
+        try:
+            return self.use_case.execute(request)
+        except NotFoundException as e:
+            raise HTTPException(status_code=404, detail=e.message)
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
+        
+@router.get("/classes/user", response_model=List[GetAllClassesByUserResponse])
+async def get_all_classes_by_user(
+    user_id: str,
+    token_user: TokenUser = Security(RequirePermission(PermissionLevelEnum.ADMIN or PermissionLevelEnum.USER or PermissionLevelEnum.PROFESSOR))
+):
+    use_case = UseCase()
+    controller = Controller(use_case=use_case)
+    return controller.handle(GetAllClassesByUserRequest(user_id=user_id))
