b944c9f034276a10d493d73082fe64caa6799a12
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-06-10 00:30:01 -0300

    Refactor ClassModel to remove pdf_url and last_access_class attributes, ensuring manager_id is included in from_orm, to_orm, and to_dict methods

--- app/domain/entities/classes.py
+++ app/domain/entities/classes.py
@@ -1,37 +1,37 @@
+from datetime import datetime
 from typing import Self, Type
 from pydantic import BaseModel
-from app.models.models import ClassModel
+from app.models.models import ClassModel as ClassModelORM
 
 
 class ClassModel(BaseModel):
     class_id: str
     group_id: str
+    manager_id: str
     title: str
-    pdf_url: str
     status: bool
-    last_access_class: str
-    created_at: str
+    created_at: datetime
     order: int
+    
     @classmethod  
-    def from_orm(cls, classes: Type[ClassModel]) -> Self:
+    def from_orm(cls, classes: Type[ClassModelORM]) -> Self:
         return cls(
             class_id=classes.class_id,
             group_id=classes.group_id,
+            manager_id=classes.manager_id,
             title=classes.title,
-            pdf_url=classes.pdf_url,
             status=classes.status,
-            last_access_class=classes.last_access_class,
             created_at=classes.created_at,
             order=classes.order
         )
-    def to_orm(self) -> ClassModel:
+        
+    def to_orm(self) -> ClassModelORM:
         return ClassModel(
             class_id=self.class_id,
             group_id=self.group_id,
+            manager_id=self.manager_id,
             title=self.title,
-            pdf_url=self.pdf_url,
             status=self.status,
-            last_access_class=self.last_access_class,
             created_at=self.created_at,
             order=self.order
         )
@@ -43,15 +43,14 @@ class ClassModel(BaseModel):
         classes = {
             "class_id": self.class_id,
             "group_id": self.group_id,
+            "manager_id": self.manager_id,
             "title": self.title,
-            "pdf_url": self.pdf_url,
             "status": self.status,
-            "last_access_class": self.last_access_class,
             "created_at": self.created_at,
             "order": self.order
         }
         
         for key in exclude:
-            classes.pop(key, None)  # Use pop to safely remove keys
+            classes.pop(key, None)
         
         return classes
