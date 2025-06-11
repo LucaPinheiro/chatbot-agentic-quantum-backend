4b98db1832f14dff428387e1ecc50a05d8e28169
Breno1101 <brenocoutinhorodrigues@gmail.com>
2025-06-11 18:59:48 -0300

    Adiciona métodos para gerenciar inscrições de usuários em grupos, incluindo verificação de duplicatas e remoção de usuários.

--- app/infra/repositories/group_enrollment_postgres.py
+++ app/infra/repositories/group_enrollment_postgres.py
@@ -1,3 +1,4 @@
+from typing import List
 from app.models.models import GroupEnrollment
 
 
@@ -17,4 +18,34 @@ class GroupEnrollmentPostgres:
             GroupEnrollment.group_id == group_id
         ).all()
         return [student.student_id for student in students]
+    
+    def get_enrollment_by_student_id(self, student_id: str) -> GroupEnrollment | None:
+        return self.db.query(GroupEnrollment).filter(GroupEnrollment.student_id == student_id).first()
+
+    def delete_user_from_group(self, user_id: str) -> None:
+        self.db.query(GroupEnrollment).filter(
+            GroupEnrollment.student_id == user_id
+        ).delete()
+        self.db.commit()
+        return {"message": "User removed from group successfully"}
+    
+    def add_user_group(self, users_id: List[str], group_id: str) -> List[GroupEnrollment]:
+        new_enrollments = []
+
+        for user_id in users_id:
+            # Verifica se a inscrição já existe para evitar duplicatas
+            existing = self.db.query(GroupEnrollment).filter_by(student_id=users_id, group_id=group_id).first()
+            if existing:
+                continue  # já está inscrito, pula
+
+            enrollment = GroupEnrollment(
+                student_id=user_id,
+                group_id=group_id
+            )
+            self.db.add(enrollment)
+            new_enrollments.append(enrollment)
+
+        self.db.commit()
+        return new_enrollments
+
 
