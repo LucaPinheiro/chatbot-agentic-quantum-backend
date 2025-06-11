ec0ba9dd3196e101e116ad05b4ff148b03199ce4
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-06-11 19:39:54 -0300

    Add GetMyProgressRequest and GetMyProgressResponse models for progress tracking

---
+++ app/schemas/get_my_progress.py
@@ -0,0 +1,16 @@
+from typing import List
+from pydantic import BaseModel
+
+class GetMyProgressRequest(BaseModel):
+    group_id: str
+
+class ClassProgressResponse(BaseModel):
+    class_id: str
+    title: str
+    progresso: str  
+
+class GetMyProgressResponse(BaseModel):
+    user_id: str
+    name: str
+    group_id: str
+    classes: List[ClassProgressResponse]
