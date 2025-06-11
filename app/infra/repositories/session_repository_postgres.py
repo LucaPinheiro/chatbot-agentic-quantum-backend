da350802a2f50dee5405b6f8da0fd86cfff1dbef
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-06-10 23:08:24 -0300

    Remove commented-out create_session method and ensure create_sessions method is properly defined for batch session creation

--- app/infra/repositories/session_repository_postgres.py
+++ app/infra/repositories/session_repository_postgres.py
@@ -10,12 +10,13 @@ class SessionRepositoryPostgres(ISessionRepository):
     def __init__(self, db: Session):
         self.db = db
 
-    # def create_session(self, session: Session) -> Session:
-    #     session_orm = session.to_orm()
-    #     self.db.add(session_orm)
-    #     self.db.commit()
-    #     self.db.refresh(session_orm)
-    #     return Session.from_orm(session_orm)
+    def create_sessions(self, sessions: List[Session]) -> None:
+        print("entrei no create_sessions")
+        orm_objects = [session.to_orm() for session in sessions]
+        self.db.add_all(orm_objects)
+        print("antes do commit")
+        self.db.commit()
+        print("depois do commit")
 
     def get_session_by_id(self, session_id: str) -> Optional[Session]:
         session = self.db.query(SessionModel).filter(SessionModel.session_id == session_id).first()
