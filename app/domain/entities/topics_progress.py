5d31763a16f7baa4ea8c01760337eb37178557b3
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-06-10 23:07:31 -0300

    Fix type annotations in TopicProgress model methods to use TopicProgressORM

--- app/domain/entities/topics_progress.py
+++ app/domain/entities/topics_progress.py
@@ -1,6 +1,6 @@
 from typing import Self, Type
 from pydantic import BaseModel
-from app.models.models import Group, GroupEnrollment, TopicProgress
+from app.models.models import TopicProgress as TopicProgressORM
 
 
 class TopicProgress(BaseModel):
@@ -9,7 +9,7 @@ class TopicProgress(BaseModel):
     user_id: str
     flag: bool
     @classmethod
-    def from_orm(cls, topics_progress: Type[TopicProgress]) -> Self:
+    def from_orm(cls, topics_progress: Type[TopicProgressORM]) -> Self:
         return cls(
             topic_progress_id = topics_progress.topic_progress_id,
             class_topics_id = topics_progress.class_topics_id, 
@@ -17,8 +17,8 @@ class TopicProgress(BaseModel):
             flag = topics_progress.flag
         )
     
-    def to_orm(self) -> TopicProgress:
-        return TopicProgress(
+    def to_orm(self) -> TopicProgressORM:
+        return TopicProgressORM(
             topic_progress_id = self.topic_progress_id,
             class_topics_id = self.class_topics_id, 
             user_id = self.user_id,
