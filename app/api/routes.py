9900bc9177981f43092a1da0a4c2884833ccbe78
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2025-06-11 19:40:00 -0300

    Add get_my_progress router to API routes

--- app/api/routes.py
+++ app/api/routes.py
@@ -6,6 +6,7 @@ from app.api.endpoints.user.create_user import router as create_user_router
 from app.api.endpoints.user.login import router as login_router
 from app.api.endpoints.user.get_user import router as get_user_router
 from app.api.endpoints.user.get_all_users import router as get_all_users_router
+from app.api.endpoints.user.get_my_progress import router as get_my_progress_router
 from app.api.endpoints.classes.get_classes_by_group import router as get_classes_by_group_router
 from app.api.endpoints.chatbot.create_message import router as create_message_router
 from app.api.endpoints.chatbot.get_chat_history import router as get_chat_history_router
@@ -60,7 +61,8 @@ router_list = [
     get_progress_percentual_by_group_router,
     get_all_classes_by_user_router,
     create_class_router,
-    get_session_router
+    get_session_router,
+    get_my_progress_router
 ]
 
 for router in router_list:
